70fab855e882821296c705676bd74dcbd72df910
Brian Saunders <brian.saunders84@gmail.com>
2015-01-31 12:04:10 -0800

    Added SendPicViewController with uploadPicture method tied to a target/action pair with an upload button

---
+++ Pic in a Box/SendPicViewController.h
@@ -0,0 +1,14 @@
+//
+//  SendPicViewController.h
+//  Pic in a Box
+//
+//  Created by Brian Saunders on 1/31/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendPicViewController : UIViewController
+
+- (IBAction)uploadPicture:(id)sender;
+@end
---
+++ Pic in a Box/SendPicViewController.m
@@ -0,0 +1,40 @@
+//
+//  SendPicViewController.m
+//  Pic in a Box
+//
+//  Created by Brian Saunders on 1/31/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+#import "SendPicViewController.h"
+
+@interface SendPicViewController ()
+
+@end
+
+@implementation SendPicViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)uploadPicture:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+@end
