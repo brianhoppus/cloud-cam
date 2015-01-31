e153848be3459e10a25fdfdb883cfc7b87d4a2f2
Brian Saunders <brian.saunders84@gmail.com>
2015-01-30 16:31:31 -0800

    Added PicViewController with associated xib

---
+++ Pic in a Box/PicCaptureViewController.m
@@ -0,0 +1,37 @@
+//
+//  PicCaptureViewController.m
+//  Pic in a Box
+//
+//  Created by Brian Saunders on 1/30/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+#import "PicCaptureViewController.h"
+
+@interface PicCaptureViewController ()
+
+@end
+
+@implementation PicCaptureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
