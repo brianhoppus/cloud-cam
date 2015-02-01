8660ec2c9d29bfb8957cddf106ec82a50368bfcd
Brian Saunders <brian.saunders84@gmail.com>
2015-01-31 18:20:48 -0800

    Made  SendPicViewController the delegate for nameField and restClient, and implemented uploadPicture

--- Pic in a Box/SendPicViewController.m
+++ Pic in a Box/SendPicViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "SendPicViewController.h"
+#import <DropboxSDK/DropboxSDK.h>
 
-@interface SendPicViewController ()
+@interface SendPicViewController () <UITextFieldDelegate, DBRestClientDelegate>
+
+@property (strong, nonatomic) DBRestClient *restClient;
 
 @end
 
@@ -17,6 +20,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.restClient = [[DBRestClient alloc] initWithSession:[DBSession sharedSession]];
+    self.restClient.delegate = self;
+    self.nameField.delegate = self;
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -41,6 +48,38 @@
 }
 
 - (IBAction)uploadPicture:(id)sender {
+    NSData *data = UIImageJPEGRepresentation(self.image, 1.0);
+    NSString *fileNameWithExtention = @"";
+    
+    if ([self.nameField.text isEqualToString:@""]) {
+        fileNameWithExtention = [self.nameField.text stringByAppendingString:@"NoName.png"];
+    } else {
+        fileNameWithExtention = [self.nameField.text stringByAppendingString:@".png"];
+    }
+
+    NSString *file = [NSTemporaryDirectory() stringByAppendingPathComponent:fileNameWithExtention];
+    [data writeToFile:file atomically:YES];
+    
+    // Upload to Dropbox
+    NSString *destinationDir = @"/";
+    [self.restClient uploadFile:fileNameWithExtention
+                         toPath:destinationDir
+                  withParentRev:nil
+                       fromPath:file];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.nameField resignFirstResponder];
+    return YES;
+}
+
+- (void)restClient:(DBRestClient *)client
+      uploadedFile:(NSString *)destPath
+              from:(NSString *)srcPath
+          metadata:(DBMetadata *)metadata
+{
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
+
 @end
