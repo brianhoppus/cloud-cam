b00f219709c06f9552a1f16c5f53efd26723c0e8
Brian Saunders <brian.saunders84@gmail.com>
2015-02-15 09:11:54 -0800

    Added Settings class to handle the storage and extraction of user preferences

---
+++ Pic in a Box/Settings.h
@@ -0,0 +1,13 @@
+//
+//  Settings.h
+//  Pic in a Box
+//
+//  Created by Brian Saunders on 2/15/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Settings : NSObject
+
+@end
---
+++ Pic in a Box/Settings.m
@@ -0,0 +1,13 @@
+//
+//  Settings.m
+//  Pic in a Box
+//
+//  Created by Brian Saunders on 2/15/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+#import "Settings.h"
+
+@implementation Settings
+
+@end
